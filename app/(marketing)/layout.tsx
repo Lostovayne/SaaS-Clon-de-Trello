1698b77aef68e429dfb6b0913dc1b3a0c0e9c640
LostoVayne <moises.barrera.devmail.com>
2024-02-14 20:44:50 -0300

    add clone trello

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="min-h-screen bg-slate-100">
+            <Navbar />
+            <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+            <Footer />
+        </div>
+    );
+}
