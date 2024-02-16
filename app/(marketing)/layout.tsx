3d0395b6df004c329271542f720944be6cc9007d
LostoVayne <moises.barrera.devmail.com>
2024-02-16 01:25:16 -0300

    add auth clerk

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./_components/navbar";
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen bg-slate-100">
+        <div className="h-full bg-slate-100">
             <Navbar />
             <main className="pt-40 pb-20 bg-slate-100">{children}</main>
             <Footer />
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Taskify",
+    description: "The ultimate task management app for teams.",
+};
