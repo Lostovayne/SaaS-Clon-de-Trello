7f01485e1c9d0c5074020feaf22e0c3cf3d90d5a
LostoVayne <moises.barrera.devmail.com>
2024-02-21 02:26:45 -0300

    agregado el layout de la organizacion

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
 import { Navbar } from "./_components/navbar";
 
 export const metadata = {
-    title: "Dashboard",
-    description: "Dashboard users",
+  title: "Dashboard",
+  description: "Dashboard users",
 };
 export default function DashBoardLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="h-full ">
-            <Navbar />
-            {children}
-        </div>
-    );
+  return (
+    <div className="h-full ">
+      <Navbar />
+      {children}
+    </div>
+  );
 }
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,10 @@
+import { OrgControl } from "./_components/org-control";
+
+export default function OrganizationIdLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+}
